e4c39cf53a15374d5836c7a2a3536ab5e5792ba0
Raghuveera Betina <rbetina@gmail.com>
2012-05-27 17:32:54 -0500

    Make necessary changes to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest
+  attr_accessible :email, :password, :password_confirmation
+  has_secure_password
+  validates_uniqueness_of :email
 end
