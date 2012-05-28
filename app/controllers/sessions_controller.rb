31c46f8596b7d0c05b1e6d0ccf463b4c25e9c26a
Raghuveera Betina <rbetina@gmail.com>
2012-05-27 20:06:50 -0500

    Make the sessions#create method check the provided email/password combo.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
-  def new
+  def new
   end
   
   def create
+    @email = params[:email]
+    user = User.find_by_email(@email)
+    if user && user.authenticate(params[:password])
+      session[:user_id] == user.id
+      redirect_to user_url(user), :notice => "Signed in as #{@email}."
+    else
+      flash[:notice] = 'Sign-in unsuccessful.'
+      render 'new'
+    end
   end
   
   def destroy
